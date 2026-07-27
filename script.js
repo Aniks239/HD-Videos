baca9b73797f684b896e59046281a287a686efcb
tera Save <aniketgiri385@gmail.com>
2026-07-27 20:15:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-const feed = "https://teraasave.blogspot.com/feeds/posts/default?alt=json-in-script&callback=?";
-
-fetch(feed)
-.then(response => response.json())
+fetch("/api/feed")
+.then(res => res.json())
 .then(data => {
 
-const posts = data.feed.entry;
+const posts = data.feed.entry || [];
+
 let html = "";
 
-posts.forEach(post => {
+posts.forEach(post=>{
 
 const title = post.title.$t;
 
-const link = post.link.find(l => l.rel === "alternate").href;
+const link = post.link.find(x=>x.rel==="alternate").href;
 
 const image = post.media$thumbnail
 ? post.media$thumbnail.url.replace("s72-c","s500")
-: "https://via.placeholder.com/500x300?text=No+Image";
+: "";
 
 html += `
 <div class="card">
@@ -32,7 +31,7 @@ html += `
 document.getElementById("posts").innerHTML = html;
 
 })
-.catch(err=>{
-document.getElementById("posts").innerHTML="Error Loading Posts";
-console.log(err);
+.catch(e=>{
+document.getElementById("posts").innerHTML="Error Loading";
+console.log(e);
 });
