084b4f2cc00cc3ea9c3b63132b0251a9aaaea214
tera Save <aniketgiri385@gmail.com>
2026-07-27 17:04:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,42 @@
-document.getElementById("posts").innerHTML =
-"<h2>Website Connected Successfully ✅</h2>";
+const feed =
+"https://teraasave.blogspot.com/feeds/posts/default?alt=json&max-results=20";
+
+fetch(feed)
+.then(res => res.json())
+.then(data => {
+
+const posts = data.feed.entry;
+
+let html = "";
+
+posts.forEach(post=>{
+
+let title = post.title.$t;
+
+let link = post.link.find(l=>l.rel=="alternate").href;
+
+let image = "";
+
+if(post.media$thumbnail){
+image = post.media$thumbnail.url.replace("s72-c","s500");
+}
+
+html += `
+<div class="card">
+
+<img src="${image}" width="100%">
+
+<h2>${title}</h2>
+
+<a href="${link}" target="_blank">
+<button>Watch Now</button>
+</a>
+
+</div>
+`;
+
+});
+
+document.getElementById("posts").innerHTML = html;
+
+});
