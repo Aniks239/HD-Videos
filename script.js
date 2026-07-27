50e6e90fc8443b861a6e1226071bbdef8b3bd2a3
tera Save <aniketgiri385@gmail.com>
2026-07-27 17:13:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,29 @@
-const feed =
-"https://teraasave.blogspot.com/feeds/posts/default?alt=json&max-results=20";
+const feed = "https://teraasave.blogspot.com/feeds/posts/default?alt=json&max-results=20";
 
 fetch(feed)
-.then(res => res.json())
+.then(response => response.json())
 .then(data => {
 
 const posts = data.feed.entry;
-
 let html = "";
 
-posts.forEach(post=>{
+posts.forEach(post => {
 
-let title = post.title.$t;
+const title = post.title.$t;
 
-let link = post.link.find(l=>l.rel=="alternate").href;
+const link = post.link.find(l => l.rel === "alternate").href;
 
-let image = "";
-
-if(post.media$thumbnail){
-image = post.media$thumbnail.url.replace("s72-c","s500");
-}
+const image = post.media$thumbnail
+? post.media$thumbnail.url.replace("s72-c","s500")
+: "https://via.placeholder.com/500x300?text=No+Image";
 
 html += `
 <div class="card">
-
-<img src="${image}" width="100%">
-
+<img src="${image}">
 <h2>${title}</h2>
-
-<a href="${link}" target="_blank">
+<a href="${link}">
 <button>Watch Now</button>
 </a>
-
 </div>
 `;
 
@@ -39,4 +31,8 @@ html += `
 
 document.getElementById("posts").innerHTML = html;
 
+})
+.catch(err=>{
+document.getElementById("posts").innerHTML="Error Loading Posts";
+console.log(err);
 });
